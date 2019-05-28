9e9ddd9611b418ea1b251ec2cfc332a67190665a
KapilDSingh <36441505+KapilDSingh@users.noreply.github.com>
2019-05-28 17:25:26 -0400

    Add files via upload

---
+++ Controllers/ChartController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using SmartAmperesServer.DataStorage;
+using SmartAmperesServer.HubConfig;
+using SmartAmperesServer.TimerFeatures;
+
+namespace SmartAmperesServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChartController : ControllerBase
+    {
+        private IHubContext<ChartHub> _hub;
+
+        public ChartController(IHubContext<ChartHub> hub)
+        {
+            _hub = hub;
+        }
+
+        public IActionResult Get()
+        {
+            var timerManager = new TimerManager(() => _hub.Clients.All.SendAsync("transferchartdata", DataManager.GetData()));
+
+            return Ok(new { Message = "Request Completed" });
+        }
+    }
+}
